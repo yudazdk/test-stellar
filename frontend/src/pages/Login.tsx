470f682158e2c8e79a19ee285377d95e74af3247
Yehuda Zadik <yehudaz@telefire.com>
2026-01-01 15:54:04 +0200

    Frontend - Improve import of hooks

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { useAuth } from '@/hooks/useAuth';
+import { useAuth } from '@/hooks';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
